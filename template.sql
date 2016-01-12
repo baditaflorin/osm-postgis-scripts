b421ff2cfdd0e4effa25a4ac495b55a7fea5207d
baditaflorin <baditaflorin@gmail.com>
2016-01-12 14:40:27 +0200

    Basic structure of a sql script file

---
+++ template.sql
@@ -0,0 +1,12 @@
+-- #Version: ______
+-- #Author: __________________________________
+-- #Date: DD.MM.YY
+-- #Website: __________________________________
+-- #Email: __________________________________
+/* #Example of Use: _________________________________________________________ */
+-- #Start Code
+-- # Comment about the below line ( Example : " This will select all the rows from the table users )
+select * from users
+-- # Comment about the below line ( Example : " This command limit the output to just the first 10 rows )
+limit 10
+-- #End Code
