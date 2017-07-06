f04f34cab475076c757423d8f4df8d9245b83cc2
baditaflorin <baditaflorin@gmail.com>
2017-07-06 17:32:47 +0300

    osm2psgql_qgis_timeseries.sql

---
+++ osm2psgql_qgis_timeseries.sql
@@ -0,0 +1,12 @@
+select ROW_NUMBER() over () as unique_id,
+tags->'osm_user' As osm_user,
+(tags->'osm_timestamp')::timestamp with time zone As osm_timestamp,
+
+(tags->'osm_timestamp')::timestamp with time zone - interval '1 month' offset_one_month_tstamp,
+(tags->'osm_timestamp')::timestamp with time zone - interval '7 days' offset_one_week_tstamp,
+(tags->'osm_timestamp')::timestamp with time zone - interval '1 days' offset_one_day_tstamp,
+
+way geom
+
+from planet_osm_line
+--limit 5
