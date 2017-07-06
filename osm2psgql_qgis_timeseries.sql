862b8c63184d2aa77c2e885d7d1332de28d34a68
baditaflorin <baditaflorin@gmail.com>
2017-07-06 17:34:57 +0300

    Update osm2psgql_qgis_timeseries.sql

--- osm2psgql_qgis_timeseries.sql
+++ osm2psgql_qgis_timeseries.sql
@@ -6,6 +6,10 @@ tags->'osm_user' As osm_user,
 (tags->'osm_timestamp')::timestamp with time zone - interval '7 days' offset_one_week_tstamp,
 (tags->'osm_timestamp')::timestamp with time zone - interval '1 days' offset_one_day_tstamp,
 
+(tags->'osm_timestamp')::timestamp with time zone + interval '1 month' offset_plus_one_month_tstamp,
+(tags->'osm_timestamp')::timestamp with time zone + interval '7 days' offset_plus_one_week_tstamp,
+(tags->'osm_timestamp')::timestamp with time zone + interval '1 days' offset_plus_one_day_tstamp,
+
 way geom
 
 from planet_osm_line
